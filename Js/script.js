5ca67b3d1ab956aa30290cb7d245668f5466a98b
Kuat <amirkhanovkuat@gmail.com>
2021-02-26 01:07:26 +0600

    Script.js modified, added conditions to prompt

--- Js/script.js
+++ Js/script.js
@@ -10,36 +10,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
 console.log(personalMovieDB);
 
+for (let i = 0; i < 2; i += 1) {
 
-/* Задание на урок:
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его', '');
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
+    if (a !== null && a !== '' && b !== null && b !== '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('Error!');
+        i -= 1;
     }
+    
+}
 
-Проверить, чтобы все работало без ошибок в консоли */
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30 ) {
+    console.log('Вы киноман!');
+} else {
+    console.log('Произошла ошибка');
+}
